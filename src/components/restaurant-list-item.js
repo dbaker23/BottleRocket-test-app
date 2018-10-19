276acbac87dd67a1e356ea2b8d94e22ea687cb47
Derek <derekjohnbaker23@gmail.com>
2018-10-18 19:41:51 -0500

    fixed scrolling

--- src/components/restaurant-list-item.js
+++ src/components/restaurant-list-item.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+const RestaurantListItem =( {restaurant, onSelect} ) => {    
+
+    return(
+        <li>
+
+        </li>
+    );
+};
+
+export default RestaurantListItem;
--- src/components/restaurant-list.js
+++ src/components/restaurant-list.js
@@ -2,14 +2,49 @@ import React, { Component } from 'react';
 import RestaurantListItem from './restaurant-list-item';
 
 const RestaurantList = ( props ) => {
+    
+
     return ( 
-        <ul className="restaurant-ul">
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-        </ul>
+        <div className='scroll-div'>
+            <ul className="restaurant-ul">
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </div>
     );
 };
 
